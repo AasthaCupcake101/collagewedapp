6cf87565ff8d5dce369fb7b62011141b0cd70477
AasthaCupcake101 <129122504+AasthaCupcake101@users.noreply.github.com>
2023-06-19 18:55:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+
+}
+recognition.onresult= function run (event)
+{
+    console.log(event);
+  var conent=event.results[0][0].transcript
+  document.getElementById ("textbox").innerHTML=conent
+  speak()
+}
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value 
+    var utterThis= new SpeechSynthesisUtterance(speak_data)
+    synth.speak(utterThis)
+   Webcam.attach(camera)
+if (speak_data=="Take my selfie.") {
+  setTimeout(() => {
+    takesnapshot()
+  },3000);
+  
+}
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format: 'png',
+    png_quality:90
+    
+})
+camera=document.getElementById("camera")
+function takesnapshot(){
+    Webcam.snap(function(data){
+        document.getElementById("selfieimg").src=data
+    })
+}
